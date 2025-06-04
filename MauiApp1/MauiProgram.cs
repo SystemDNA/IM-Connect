b3f39b1a07d6850770ab9cf7fae9e6bd75615856
Thanusha Chowdary <thanusha.c@systemsdna.com>
2025-06-04 08:09:18 +0530

    added login

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -18,6 +18,8 @@ namespace MauiApp1
             builder.Services.AddSingleton<BiometricAuthService>();
             builder.Services.AddSingleton<UserLocationService>();
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://platformfactory.uk:6888/") });
+
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
