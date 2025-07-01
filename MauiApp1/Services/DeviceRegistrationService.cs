78368601b892a51cf0f8bb92c04cbc6ffa337544
Narendar092 <94607025+Narendar092@users.noreply.github.com>
2025-07-01 07:48:44 +0530

    Push Notification code

---
+++ MauiApp1/Services/DeviceRegistrationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class DeviceRegistrationService : IDeviceRegistrationService
+    {
+
+        public DeviceRegistrationService()
+        {
+
+        }
+
+        public async Task SendDeviceInfoToServerAsync(string deviceId, string firebaseToken, string platform)
+        {
+            var payload = new
+            {
+                deviceId,
+                firebaseToken,
+                platform
+            };
+
+        }
+    }
+}
---
+++ MauiApp1/Services/FirebaseMessagingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Xamarin.Google.Crypto.Tink.Shaded.Protobuf;
+
+namespace MauiApp1.Services
+{
+    public class FirebaseTokenRetriever : IFirebaseTokenRetriever
+    {
+        private readonly IDeviceRegistrationService _deviceRegistrationService;
+
+
+        private const string TokenKey = "FirebaseToken";
+
+        public FirebaseTokenRetriever(IDeviceRegistrationService deviceRegistrationService)
+        {
+            _deviceRegistrationService = deviceRegistrationService;
+        }
+        // This property holds the Firebase token. 
+        // It has a public get accessor but a private set accessor, 
+        // meaning that it can be publicly read, but only changed internally by the class.
+        public string Token { get; private set; }
+
+        // This method allows for the saving of a new Firebase token.
+        // It takes in a string token and assigns it to the Token property.
+        public void SaveToken(string token)
+        {
+            Token = token;
+            Preferences.Set(TokenKey, token);
+
+
+            var deviceId = DeviceIdService.GetOrCreateDeviceId();
+            var platform = DeviceIdService.GetPlatformName();
+            _ = _deviceRegistrationService.SendDeviceInfoToServerAsync(deviceId, token, platform);
+        }
+
+        public string GetToken()
+        {
+            return Preferences.Get(TokenKey, string.Empty);
+        }
+    }
+}
