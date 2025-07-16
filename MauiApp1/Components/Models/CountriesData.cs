e73fffa05bf088299322074f3d71bfec1c42d7e2
Narendar092 <94607025+Narendar092@users.noreply.github.com>
2025-07-16 13:20:28 +0530

    Changes

--- MauiApp1/Components/Models/CountriesData.cs
+++ MauiApp1/Components/Models/CountriesData.cs
@@ -10,7 +10,7 @@ namespace MauiApp1.Components.Models
     {
         public int id { get; set; }
         public string sName { get; set; } = string.Empty;
-        public int fkiCountryID { get; set; }
+        public int fKiCountryID { get; set; }
         public int? iStatus { get; set; }
         public string sImages { get; set; } = string.Empty;
         public string? imagePath { get; set; }
